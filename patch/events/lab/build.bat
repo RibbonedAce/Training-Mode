f49e8b549e0ddbb6f80562d2af960e521ff74fe2
Tyler Gemora <tylergemora@yahoo.com>
2024-08-16 19:39:53 -0700

    Fix build for lab_css.c by injecting cssFunction symbols.

--- patch/events/lab/build.bat
+++ patch/events/lab/build.bat
@@ -15,9 +15,16 @@ call "..\..\build-core.bat"^
 
 echo Next.
 
-call "..\..\build-core.bat"^
- assets\importData.dat^
- source\lab_css.c^
- output\EvLabCSS.dat^
- %MEX_DIR%^
- "..\..\..\Additional ISO Files\TM\EvLabCSS.dat"
+echo Copying base assets file assets\importData.dat to output\EvLabCSS.dat.
+cp assets\importData.dat output\EvLabCSS.dat
+
+echo Injecting symbols from source\lab_css.c into output\EvLabCSS.dat.
+"%MEX_DIR%\MexTK.exe" -ff -i source\lab_css.c -s cssFunction -dat output\EvLabCSS.dat -t "%MEX_DIR%\cssFunction.txt" -q -ow -w -c -l "%MEX_DIR%\melee.link" -op 1
+
+echo Trimming output\EvLabCSS.dat.
+"%MEX_DIR%\MexTK.exe" -trim output\EvLabCSS.dat
+
+echo Copying output output\EvLabCSS.dat to "..\..\..\Additional ISO Files\TM\EvLabCSS.dat".
+cp output\EvLabCSS.dat "..\..\..\Additional ISO Files\TM\EvLabCSS.dat"
+
+echo.
